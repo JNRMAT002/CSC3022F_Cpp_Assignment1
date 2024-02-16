9cf9a02d1c66dd80ab24a42a877217a8fdb69fcd
JNRMAT002 <matthewjanuary@gmail.com>
2024-02-16 16:33:13 +0200

    Added complete basic program functionality, including functions for word count and letter count.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
+
+// Function to remove punctuation and other non-letters and non-digits using stringstream.
+std::string wordCleaner(std::string word) {
+    std::string cleanWord;
+    for (int i = 0; i < word.length(); i++) {
+        // If statement check if valid letter = False.
+        if ( (word[i] < 'A' || word[i] > 'Z') && (word[i] < 'a' || word[i] > 'z') && (word[i] < '0' || word[i] > '9') ) {
+            // std::cout << word[i] << " ";
+            continue;
+        }
+        
+        cleanWord += word[i];
+    }
+
+    return cleanWord;
+}
+
+// Function to count chars in each line using stringstream.
+int countChars(std::string input) {
+    std::stringstream inputSS(input);
+    std::string word;
+    int charCount = 0;
+
+    while (inputSS >> word) {
+        std::string cleanWord = wordCleaner(word);
+        charCount += cleanWord.length();
+    }
+    
+    return charCount;
+}
+
+// Function to count words in each line using stringstream.
+int countWords(std::string input) {
+    std::stringstream inputSS(input);
+    std::string word;
+    int lineWordCount = 0;
+
+    while (inputSS >> word) {
+        std::string cleanWord = wordCleaner(word);
+        if (!cleanWord.empty()) { lineWordCount++; }
+    }
+
+    return lineWordCount;
+}
 
 int main () {
-    // std::vector<std::string> files;
     std::string input;
+    int totalCharCount = 0; // Total number of character in the text file
+    int totalWordCount = 0; // Total number of words in the text file
+    int lineCount = 0; // Number of lines in the text file
 
-    while (std::cin) {
-        getline(std::cin, input);
-        std::cout << input << std::endl;
+    while (getline(std::cin, input)) {
+        if (input[0] == '>') { lineCount++; }
+        totalWordCount += countWords(input);
+        totalCharCount += countChars(input);
     }
+
+    // Printing output to the console
+    std::cout << lineCount << " " << totalWordCount << " " << totalCharCount << std::endl;
     
     return 0;
 }
